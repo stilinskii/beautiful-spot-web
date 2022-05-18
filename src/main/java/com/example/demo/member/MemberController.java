6a4bbc90afb9b06ca185ed917f7529b805370329
stilinskii <lhy1031hy@gmail.com>
2022-05-19 01:49:34 +0900

    add index.html

---
+++ src/main/java/com/example/demo/member/MemberController.java
@@ -0,0 +1,15 @@
+package com.example.demo.member;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+//
+//    @GetMapping
+//    public String hello(){
+//        return "success";
+//    }
+
+
+}
