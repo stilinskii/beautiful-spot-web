ab9a05077c2dca4000c3d27935f37a0a789307ed
stilinskii <lhy1031hy@gmail.com>
2022-05-18 23:47:31 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/LoginSystemWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSystemWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSystemWebApplication.class, args);
+	}
+
+}
