c49201af01b8a75f892614bdfacec538db22f108
stilinskii <lhy1031hy@gmail.com>
2022-06-08 03:11:50 +0900

    add article page and implement edit page

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Board;
 import com.example.demo.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,10 +14,11 @@ import java.nio.file.Paths;
 import java.sql.Blob;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/form")
+@RequestMapping("/board")
 public class FormController {
 
     @Autowired
@@ -27,7 +29,7 @@ public class FormController {
         return "board/form";
     }
 
-    @PostMapping
+    @PostMapping("/form")
     public String save(@ModelAttribute Board board,@RequestParam("image") MultipartFile image) throws IOException{
        String imageFileName = UUID.randomUUID()+"_"+image.getOriginalFilename();
        String path = System.getProperty("user.dir")+"/src/main/resources/static/images/";
@@ -43,4 +45,28 @@ public class FormController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/form")
+    public String formSubmit(Model model, @RequestParam(required = false) Integer id){
+        if(id == null){
+            model.addAttribute("board",new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
+        return "board/form";
+    }
+
+    @GetMapping("/article")
+    public String article(Model model, @RequestParam(required = false) Integer id){
+        if(id == null){
+            model.addAttribute("board",new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
+        return "board/article";
+    }
+
+
 }
