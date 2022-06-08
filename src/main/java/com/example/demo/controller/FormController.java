c0ecac3412a930cee436def3bbbf83128435b15a
stilinskii <lhy1031hy@gmail.com>
2022-06-09 01:20:34 +0900

    fix uploading issue

--- src/main/java/com/example/demo/controller/FormController.java
+++ src/main/java/com/example/demo/controller/FormController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Board;
 import com.example.demo.repository.BoardRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,7 +19,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.UUID;
-
+@Slf4j
 @Controller
 @RequestMapping("/board")
 public class FormController {
@@ -25,17 +28,25 @@ public class FormController {
     private BoardRepository boardRepository;
 
     @GetMapping
-    public String form(){
-        return "board/form";
+    public String form(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
+
+        return "board/board";
     }
 
     @PostMapping("/form")
-    public String save(@ModelAttribute Board board,@RequestParam("image") MultipartFile image) throws IOException{
-       String imageFileName = UUID.randomUUID()+"_"+image.getOriginalFilename();
+    public String formSubmit(@Valid Board board,BindingResult bindingResult, @RequestParam(value = "image",required=false) MultipartFile image) throws IOException{
+       if(bindingResult.hasErrors()){
+           log.info("bindingResult={}",bindingResult.getObjectName());
+           return "board/form";
+       }
+
+
+        String imageFileName = UUID.randomUUID()+"_"+image.getOriginalFilename();
        String path = System.getProperty("user.dir")+"/src/main/resources/static/images/";
         Path imagePath = Paths.get(path + imageFileName);
         Files.write(imagePath, image.getBytes());
-
 
         board.setFilename(imageFileName);
         board.setFilepath("/images/"+imageFileName);
@@ -44,10 +55,11 @@ public class FormController {
 
 
         return "redirect:/";
+
     }
 
     @GetMapping("/form")
-    public String formSubmit(Model model, @RequestParam(required = false) Integer id){
+    public String form(Model model, @RequestParam(required = false) Integer id){
         if(id == null){
             model.addAttribute("board",new Board());
         }else{
@@ -67,6 +79,7 @@ public class FormController {
         }
         return "board/article";
     }
+
 
 
 }
--- src/main/java/com/example/demo/model/Board.java
+++ src/main/java/com/example/demo/model/Board.java
@@ -3,7 +3,8 @@ package com.example.demo.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.sql.Blob;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 @Entity
 @Data
 @SequenceGenerator(name="Article_SEQ_GENERATOR", sequenceName="Board_SEQ", initialValue=1, allocationSize=1)
@@ -12,9 +13,15 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType. SEQUENCE, generator = "Article_SEQ_GENERATOR")
     private Integer id;
+    @NotNull
+    @Size(min=1, max=30)
     private String title;
+    @NotNull
+    @Size(min=1)
     private String content;
+
     private String filename;
+
     private String filepath;
     //작성자도 필요
 }
